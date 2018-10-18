2ad8724502881c34400cf5e37634d0975f8450b7
Edward <edward.bebbington@intercity.technology>
2018-10-18 14:45:52 +0100

    AJAX request now saves data BASED on user input into the database. date column needs to be sorted and today variable to be changed.

--- models/savecomment.php
+++ models/savecomment.php
@@ -3,16 +3,16 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "password";
+	$author = $_POST['author'];
+	$comment = $_POST['comment'];
+	$date = $_POST['dateposted'];
 
 	//create connection
 	$connection = new mysqli($servername, $username, $password, 'copytube');
-	var_dump($_POST);
-	$date = $_POST['dateposted'];
+
 	//if connection works, set variable to string of inserting data
-	//$sql = "INSERT INTO comments (author, comment, dateposted) VALUES ($_POST['author'], $_POST['comment'], $_POST['dateposted'])";
-	$sql = "INSERT INTO comments (author, comment, dateposted) VALUES 
-	('abc', 'def', '".$date."')";
-	var_dump($sql);
+	$sql = "INSERT INTO comments (author, comment, dateposted) VALUES
+	('".$author."', '".$comment."', '".$date."')";
 
 	//set this data in the database
 	$connection->query($sql);
