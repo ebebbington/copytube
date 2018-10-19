e5aac0b8b091664efee648ab0fb7d1ebe867f7fb
Edward <edward.bebbington@intercity.technology>
2018-10-19 12:58:53 +0100

    Added validation for adding a comment - user cannot exceed comment length in database.

--- copytube.js
+++ copytube.js
@@ -54,45 +54,50 @@ $(document).ready(function(){
 
 	//when the add comment button is clicked
 	$('#comment-button').on('click', function(){
-		var emptycheck = $('#comment-bar').val();
-		if (emptycheck == ""){
+
+		var description = $('#comment-bar').val();
+		var max_length = 400;
+		if (description == ""){
 			alert("Please input a comment");
 		} else {
-			//setting "description" to equal value in comment bar
-			var description = $('#comment-bar').val();
-			//setting "today" to equal todays date
-			var today = new Date();
-			var dd = today.getDate();
-			var mm = today.getMonth();
-			mm += 1; //some reason, month was 1 behind
-			var yyyy = today.getFullYear();
-			//today = dd + '/' + mm + '/' + yyyy;
-			today = yyyy + "-" + mm + "-" + dd;
+			if (description.length > max_length){
+				alert("Comment exceeds maximum characters, maximum characters is 400 and your comment is " + description.length + " characters long");
+			} else {
+				//setting "today" to equal todays date
+				var today = new Date();
+				var dd = today.getDate();
+				var mm = today.getMonth();
+				mm += 1; //some reason, month was 1 behind
+				var yyyy = today.getFullYear();
+				//today = dd + '/' + mm + '/' + yyyy;
+				today = yyyy + "-" + mm + "-" + dd;
 
-			//combing these variables into one variable to concatenate them and display them in order
-			var actualcomment = '<br>' + '<br>' + "Username: " + username + "<br>" + "Date: " + today + "<br>" + "Comment: " + description + "<br>";
-			//assign above variable to the id for the div to display
-			$('#user-comments').prepend(actualcomment);
-			//clear comment text bar
-			$('#comment-bar').val("");
-		}
-		//start of setting up ajax request by setting url to go to and data
-		$.ajax({
-			type: "POST",
-			url: "models/savecomment.php",
-			data: {
-				author: username,
-				comment: description,
-				dateposted: today
-			},
-			//if working or if not
-			success: function(response){
-				console.log('AJAX save-comment Response: AJAX request has followed through.');
-			},
-			error: function(err) {
-				console.log('AJAX save-comment Response: ERROR - Request for AJAX has not passed.');
+				//combing these variables into one variable to concatenate them and display them in order
+				var actualcomment = '<br>' + '<br>' + "Username: " + username + "<br>" + "Date: " + today + "<br>" + "Comment: " + description + "<br>";
+				//assign above variable to the id for the div to display
+				$('#user-comments').prepend(actualcomment);
+				//clear comment text bar
+				$('#comment-bar').val("");
+
+					//start of setting up ajax request by setting url to go to and data
+				$.ajax({
+				type: "POST",
+					url: "models/savecomment.php",
+					data: {
+						author: username,
+						comment: description,
+						dateposted: today
+					},
+					//if working or if not
+					success: function(response){
+						console.log('AJAX save-comment Response: AJAX request has followed through.');
+					},
+					error: function(err) {
+						console.log('AJAX save-comment Response: ERROR - Request for AJAX has not passed.');
+					}
+				}); //I can use "var_dump($_[typename])" to get props in network response which i an then do "var_dump($_POST[author])" to get value of this property
 			}
-		}); //I can use "var_dump($_[typename])" to get props in network response which i an then do "var_dump($_POST[author])" to get value of this property
+		}
 
 	})
 
