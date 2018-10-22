caaf71edb8d0e5d2057c8a223a24b18565acdb19
Edward <edward.bebbington@intercity.technology>
2018-10-22 15:49:40 +0100

    Added regions to .js to tidy code up. Cleared up a few things in .js.

--- copytube.js
+++ copytube.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-	//getting the users name by asking for an input and saving this to use for comments later
-	var username = prompt("Please enter your username below or result to 'ALT+F4'...");
+	//region Getting Username, Validation & Display Welcome Message
+	//getting a username by asking for an input and saving this to a variable
+	var username = prompt("Please enter your username below or result to closing the tab.");
 
 	//If the user ignores this and presses cancel (which equals null) or types nothing and clicks ok then window will close
 
@@ -15,11 +16,11 @@ $(document).ready(function(){
 			console.log("Alerts left: " + left);
 		}
 	}
-
 	//generates the welcome message with the users username
-	$('#welcome').text("Hello " + username + ", and welcome to CopyTube, where you will find a plagurised version of YouTube");
+	$('#welcome').text("Hello " + username + ", and welcome to CopyTube, where you will find a plagiarised version of YouTube");
+	//endregion
 
-	//object array for videos
+	//region Object Array for Videos
 	var arr = [{
 		name: 'Big Buck Bunner Trailer (2018)',
 		src: 'http://dl3.webmfiles.org/big-buck-bunny_trailer.webm',
@@ -41,8 +42,9 @@ $(document).ready(function(){
 		width: '230',
 		description: "It's a bird! It's a plane! Its... Lego?",
 	}];
+	//endregion
 
-	//pre-defining description & title and assigning these to the id for display
+	//region Pre-define and Display Titles, and Description
 	var main_vid_title = arr[0].name;
 	$('#main-video-title').text(main_vid_title);
 	var main_vid_description = arr[0].description;
@@ -51,8 +53,9 @@ $(document).ready(function(){
 	$('#rabbit-hole-vid-1-title').text(rabbit_hole_vid_1_title);
 	var rabbit_hole_vid_2_title = arr[2].name;
 	$('#rabbit-hole-vid-2-title').text(rabbit_hole_vid_2_title);
+	//endregion
 
-	//when the add comment button is clicked
+	//region When Add Comment Button is Clicked
 	$('#comment-button').on('click', function(){
 
 		var description = $('#comment-bar').val();
@@ -60,48 +63,52 @@ $(document).ready(function(){
 		if (description == ""){
 			alert("Please input a comment");
 		} else {
-			if (description.length > max_length){
-				alert("Comment exceeds maximum characters, maximum characters is 400 and your comment is " + description.length + " characters long");
-			} else {
-				//setting "today" to equal todays date
-				var today = new Date();
-				var dd = today.getDate();
-				var mm = today.getMonth();
-				mm += 1; //some reason, month was 1 behind
-				var yyyy = today.getFullYear();
-				//today = dd + '/' + mm + '/' + yyyy;
-				today = yyyy + "-" + mm + "-" + dd;
+            if (description.length > max_length) {
+                alert("Comment exceeds maximum characters, maximum characters is 400 and your comment is " + description.length + " characters long");
+            } else {
+            	//region Setting actualcomment and Displaying
+                //setting "today" to equal todays date
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth();
+                mm += 1; //some reason, month was 1 behind
+                var yyyy = today.getFullYear();
+                //today = dd + '/' + mm + '/' + yyyy;
+                today = yyyy + "-" + mm + "-" + dd;
 
-				//combing these variables into one variable to concatenate them and display them in order
-				var actualcomment = '<br>' + '<br>' + "Username: " + username + "<br>" + "Date: " + today + "<br>" + "Comment: " + description + "<br>";
-				//assign above variable to the id for the div to display
-				$('#user-comments').prepend(actualcomment);
-				//clear comment text bar
-				$('#comment-bar').val("");
+                //combing these variables into one variable to concatenate them and display them in order
+                var actualcomment = '<br>' + '<br>' + "Username: " + username + "<br>" + "Date: " + today + "<br>" + "Comment: " + description + "<br>";
+                //assign above variable to the id for the div to display
+                $('#user-comments').prepend(actualcomment);
+                //clear comment text bar
+                $('#comment-bar').val("");
+                //endregion
 
-					//start of setting up ajax request by setting url to go to and data
-				$.ajax({
-				type: "POST",
-					url: "models/savecomment.php",
-					data: {
-						author: username,
-						comment: description,
-						dateposted: today
-					},
-					//if working or if not
-					success: function(response){
-						console.log('AJAX save-comment Response: AJAX request has followed through.');
-					},
-					error: function(err) {
-						console.log('AJAX save-comment Response: ERROR - Request for AJAX has not passed.');
-					}
-				}); //I can use "var_dump($_[typename])" to get props in network response which i an then do "var_dump($_POST[author])" to get value of this property
-			}
-		}
-
+                //region AJAX save-comments Request
+                //start of setting up ajax request by setting url to go to and data
+                $.ajax({
+                    type: "POST",
+                    url: "models/savecomment.php",
+                    data: {
+                        author: username,
+                        comment: description,
+                        dateposted: today
+                    },
+                    //if working or if not
+                    success: function (response) {
+                        console.log('AJAX save-comment Response: AJAX request has followed through.');
+                    },
+                    error: function (err) {
+                        console.log('AJAX save-comment Response: ERROR - Request for AJAX has not passed.');
+                    }
+                }); //I can use "var_dump($_[typename])" to get props in network response which i an then do "var_dump($_POST[author])" to get value of this property
+                //endregion
+            }
+        }
 	})
+	//endregion
 
-	//when a rabbit hole video is clicked
+    //region When Rabit Hole is Clicked
 	$(document).on('click', '.rabbit-hole-vid',function(){
 
 		//creating varibles for titles and description
@@ -121,5 +128,7 @@ $(document).ready(function(){
 		//setting main and clicked video source
 		$('#main-video').prop('src', clicked_vid_src);
 		$(this).prop('src', main_vid_src);
+		//
 	})
+    //endregion
 })
