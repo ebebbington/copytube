c86ec053a1f2fe3a03acd09c0bf66da4d4c86b1c
Edward <edward.bebbington@intercity.technology>
2019-01-24 15:37:35 +0000

    Cleaned up comments and regions and empty lines

--- copytube.php
+++ copytube.php
@@ -2,30 +2,20 @@
 
 <html>
 
+    <!-- Set data before DOM -->
 	<head>
-
-        <!-- region Tab Title & Welcome Message -->
 		<title>CopyTube</title>
 		<h2 id="welcome"></h2>
-        <!-- endregion -->
-
-        <!-- region Links & Scripts -->
 		<!-- allows .js code to run jquery -->
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-
 		<!-- accesses bootstrap css files that makes css files much easier to use -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
 		<!-- accesses bootstrap js files that makes js files much easier to use -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
 		<!-- links my style sheet (.css) so it can be used -->
 		<link rel="stylesheet" href="copytube.css"/>
-
 		<!-- links my javascript sheet so it can be used -->
 		<script src="copytube.js"></script>
-        <!-- endregion -->
-
 	</head>
 
 	<body>
@@ -33,16 +23,12 @@
 		<div class="container">
 
 			<div class="row">
-
-				<!-- region Logo -->
+                <!-- set logo -->
 				<div class="col-xs-3">
-
 					<img id="logo" src="imageresources/CopyTube_Logo.png"/>
-
 				</div>
-                <!-- endregion -->
 
-				<!-- region Search Bar/Button -->
+				<!-- create search bar and button -->
 				<div class="col-xs-9">
 					<div id="search" class="input-group">
                         <div class="dropdown">
@@ -58,20 +44,15 @@
       					</span>
     				</div>
 				</div>
-                <!-- endregion -->
-
 			</div>
 
 			<div class="row">
-
 				<div class="col-xs-9">
-
-                        <!-- region Main Video/Title/Description -->
+                        <!-- display main video, title and description -->
 						<br>
 						<br>
 						<br>
 						<div class="my-video col-xs-12">
-							<!-- video -->
 							<video id='main-video' controls
 									autoplay
 							    	muted
@@ -82,33 +63,23 @@
 							    	height="400">
 							    Sorry, your browser doesn't support embedded videos.
 							</video>
-							<!-- main video title -->
 							<p id="main-video-title">Something More</p>
 							<br>
-							<!-- main video description -->
 							<p id="main-video-description">Watch this inspirational video as we look at all of the beautiful things inside this world</p>
 						<br>
 						<br>
 						</div>
-                        <!-- endregion -->
 
-						<!-- region Comments Title/Count/Bar & Button -->
+						<!-- create comments section -->
                         <p id="comment-title">Comments Section</p> <p id="comment-count"></p>
 						<div id="comment" class="input-group">
-							<!-- comments bar -->
 							<input id="comment-bar" type="text" class="form-control my-input" placeholder="Add a comment..."/>
       						<span class="input-group-btn">
-      							<!-- comments button -->
        							<button id="comment-button" class="btn btn-default" type="button">Add</button>
       						</span>
     					</div>
-                        <!-- endregion -->
     					<br>
-						<!-- region Display New Comment -->
                         <div id="user-comments"></div>
-                        <!-- endregion -->
-
-                        <!-- region Get Database Comments -->
                         <div id="db-comments">
                             <?php
                                 // setting variables
@@ -134,16 +105,11 @@
                                 $connection->close();
                             ?>
                         </div>
-                        <!-- endregion -->
-
 				</div>
 
-                <!-- region Rabbit Hole Videos -->
+                <!-- set rabbit hole -->
 				<div class="col-xs-3">
-
 					<div class="rabbit-holes col-xs-12">
-
-                        <!-- PHP Code Displaying Rabbit Holes -->
                         <?php
                             // setting variables
                             $servername = "localhost";
@@ -181,16 +147,9 @@
                             //close database connection
                             $connection->close();
                         ?>
-
                     </div>
-
 				</div>
-                <!-- endregion -->
-
 			</div>
-
 		</div>
-
 	</body>
-
 </html>
