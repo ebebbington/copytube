e82edc6c172fc50795aac7a876a24dd2c6a656f8
Edward <edward.bebbington@intercity.technology>
2019-01-25 13:39:48 +0000

    Part 3/3 completed formatting and refatoring of all files

--- models/get_videos.php
+++ models/get_videos.php
@@ -1,16 +1,16 @@
 <?php
-    $serverName = "localhost";
-    $username = "root";
-    $password = "password";
-    //create connection
-    $connection = new mysqli($serverName, $username, $password, 'copytube');
-    //check connection
-    if ($connection->connect_error) {
-        die("connection to database inside getvideos.php has failed: $connection->connect_error");
-    }
-    //if connection works, set variable to string, get all from this and encode it
-    $sql = "SELECT title, src, description, poster, width, height FROM videos";
-    $result = $connection->query($sql);
-    $response = $result->fetch_all(MYSQLI_ASSOC);
-    print_r(json_encode($response));
-    $connection->close();
+$serverName = "localhost";
+$username = "root";
+$password = "password";
+//create connection
+$connection = new mysqli($serverName, $username, $password, 'copytube');
+//check connection
+if ($connection->connect_error) {
+    die("connection to database inside getvideos.php has failed: $connection->connect_error");
+}
+//if connection works, set variable to string, get all from this and encode it
+$sql = "SELECT title, src, description, poster, width, height FROM videos";
+$result = $connection->query($sql);
+$response = $result->fetch_all(MYSQLI_ASSOC);
+print_r(json_encode($response));
+$connection->close();
--- models/save_comment.php
+++ models/save_comment.php
@@ -1,22 +1,21 @@
 <?php
-	//Setting variables up
-	$servername = "localhost";
-	$username = "root";
-	$password = "password";
-	$author = $_POST['author'];
-	$comment = $_POST['comment'];
-	$date = $_POST['datePosted'];
-	$videoTitle = $_POST['videoTitle'];
-	//create connection
-	$connection = new mysqli($servername, $username, $password, 'copytube');
-	//check connection
-  if ($connection->connect_error) {
+//Setting variables up
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$author = $_POST['author'];
+$comment = $_POST['comment'];
+$date = $_POST['datePosted'];
+$videoTitle = $_POST['videoTitle'];
+//create connection
+$connection = new mysqli($servername, $username, $password, 'copytube');
+//check connection
+if ($connection->connect_error) {
     die("connection to database inside savecomment.php has failed: " + $connection->connect_error);
-  }
-	//if connection works, set variable to string of inserting data
-	$sql = "INSERT INTO comments (author, comment, dateposted, title) VALUES
-	('".$author."', '".$comment."', '".$date."', '".$videoTitle."')";
-	//set this data in the database
-	$connection->query($sql);
-	$connection->close();
-?>
+}
+//if connection works, set variable to string of inserting data
+$sql = "INSERT INTO comments (author, comment, dateposted, title) VALUES
+('" . $author . "', '" . $comment . "', '" . $date . "', '" . $videoTitle . "')";
+//set this data in the database
+$connection->query($sql);
+$connection->close();
