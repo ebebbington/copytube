1575ec6d16700ea1d11b31a892fd9450cb46c92d
Edward <edward.bebbington@intercity.technology>
2019-02-06 12:59:41 +0000

    Successfully hashed passwords and now registering now works. Moving on to login

--- register/register.js
+++ register/register.js
@@ -1,41 +1,21 @@
 /* global $, alert */
 
-function validation () {
-  const [ username, password, maxLength ] = [ $('#register-username').val(), $('#register-password').val(), 40 ]
-  if (username === '' || username > maxLength || username.trim().length === 0 || username === null || username === undefined) {
-    alert('Enter correct credentials')
-    return false
-  } else {
-    if (password === '' || password > maxLength || password.trim().length === 0 || password === null || password === undefined) {
-      alert('Enter correct credentials')
-      return false
-    } else {
-      return true
-    }
-  }
-}
-
 $(document).ready(function () {
   $(document).on('click', '#register-button', function () {
-    const output = validation()
-    console.log(output)
-    console.log($('#login-username').val())
-    if (output === false) {
-    } else {
-      $.ajax({
-        type: 'POST',
-        url: 'register.php', // todo :: create server side validation
-        data: {
-          username: $('#login-username').val(),
-          password: $('#login-password').val()
-        },
-        success: function () {
-          window.location.replace('http://localhost/copytube/login/login.html')
-        },
-        error: function () {
-          alert('Provide correct credentials')
-        }
-      })
-    }
+    $.ajax({
+      type: 'POST',
+      url: 'register.php',
+      data: {
+        username: $('#register-username').val(),
+        password: $('#register-password').val()
+      },
+      success: function () {
+        alert('Account successfully registered')
+        window.location.replace('http://localhost/copytube/login/login.html')
+      },
+      error: function () {
+        alert('Provide correct credentials')
+      }
+    })
   })
 })
--- register/register.php
+++ register/register.php
@@ -11,8 +11,20 @@ $username = "root";
 $password = "password";
 $usernameInput = $_POST['username'];
 $passwordInput = $_POST['password'];
-// Validate
-// todo :: validate server side here
+$maxLength = 40;
+// todo :: Validate server side
+/* if ($usernameInput === '' || $usernameInput >= ($maxLength + 1) || $usernameInput.str.length === 0 || $usernameInput === null || $usernameInput === undefined) {
+    alert('Enter correct credentials');
+    return false;
+  } else {
+      if (password === '' || password > maxLength || password.trim().length === 0 || password === null || password === undefined) {
+          alert('Enter correct credentials');
+      return false;
+    } else {
+          return true;
+    }
+  }
+} */
 // Hash
 $hash = password_hash($passwordInput, PASSWORD_BCRYPT);
 //create connection
@@ -22,7 +34,8 @@ if ($connection->connect_error) {
     die("connection to database inside savecomment.php has failed: " + $connection->connect_error);
 }
 //if connection works, set variable to string of inserting data
-$sql = "INSERT INTO users (username, password, loggedIn) VALUES ($usernameInput, $hash, 1)";
+$sql = "INSERT INTO users (username, password, loggedIn) VALUES ('$usernameInput', '$hash', 1)";
+
 //set this data in the database
 $connection->query($sql);
 $connection->close();
