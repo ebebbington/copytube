aa3f1c77a974df388620b17dfb4ae2d0e26bb9d6
Edward <edward.bebbington@intercity.technology>
2019-02-07 23:48:06 +0000

    Successfully got the code to work that checks if password is correct or not.

--- login/login.php
+++ login/login.php
@@ -10,16 +10,15 @@
 $serverName = "localhost";
 $username = "root";
 $password = "password";
-$usernameInput = $_GET['username'];
-$passwordInput = $_GET['password'];
+$maxLength = 40;
+$usernameInput = $_POST['username'];
+$passwordInput = $_POST['password'];
 
 // Validation
 if ($usernameInput === '' || $usernameInput >= ($maxLength + 1) || trim($usernameInput) === 0 || $usernameInput === null) {
     print_r(false);
 } else {
-    if ($passwordInput === '' || $passwordInput >= ($maxLength + 1) || trim($passwordInput) === 0
-      || $passwordInput === null
-    ) {
+    if ($passwordInput === '' || $passwordInput >= ($maxLength + 1) || trim($passwordInput) === 0 || $passwordInput === null) {
         print_r(false);
     } else {
         //create connection to db
@@ -29,22 +28,25 @@ if ($usernameInput === '' || $usernameInput >= ($maxLength + 1) || trim($usernam
             die("connection failed: " . $connection->connect_error);
         }
         //Get password from db
-        $sql = "SELECT username, password FROM `users` WHERE username='$usernameInput'";
+        $sql = "SELECT username, password FROM users WHERE username='$usernameInput'";
         //$result = query of $sql
         $result = $connection->query($sql);
         //If query fails, die. If not then get results
         if ($result == false){
-            die("PHP Response SQL: The query of sql is false.
-        Result is: $result.
-        Sql is: $sql.");
         } else {
             $response = $result->fetch_all(MYSQLI_ASSOC);
-            $connection->close();
-        }
-        //Compare DB and User Password
-        if (password_verify($passwordInput, $response[0]['password'])){
-            $sql = "UPDATE `users` SET loggedIn = 0 WHERE username='$usernameInput'";
-            print_r(true);
+            //Compare DB and User Password
+            $passwordHash = password_hash($passwordInput, PASSWORD_BCRYPT);
+            if (password_verify($passwordInput, $response[0]['password'])) {
+                echo "<script>alert('password verification succeeded');</script>";
+                $sql = "UPDATE users SET loggedIn = 0 WHERE username = '$usernameInput'";
+                $connection->query($sql);
+                $connection->close();
+                $open = "<script>window.open('http://localhost/copytube/copytube.php')</script>";
+                echo $open;
+            } else {
+                echo "<script>alert('password not verified = wrong');</script>";
+            }
         }
     }
 }
