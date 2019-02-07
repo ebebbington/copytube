0fdc024e6a11180422b66893740686fe159c77ea
Edward <edward.bebbington@intercity.technology>
2019-02-07 23:49:54 +0000

    Removed submit click event as it was useless

--- login/login.js
+++ login/login.js
@@ -1,29 +1,6 @@
-/* global $, alert */
-
-function test () {
-  console.log('test')
-}
+/* global $ */
 
 $(document).ready(function () {
-  $(document).on('click', '#login-button', function () {
-    $.ajax({
-      type: 'GET',
-      url: 'login.php',
-      data: {
-        username: $('#login-username').val(),
-        password: $('#login-password').val()
-      },
-      success: function (response) {
-        if (response === true) {
-          alert('SUCCESSFULLY LOGGED IN')
-          window.location.replace('http://localhost/copytube/copytube.php')
-        }
-      },
-      error: function () {
-        alert('Provide correct credentials')
-      }
-    })
-  })
   // On click of registering
   $(document).on('click', '#register-new-account', function () {
     window.location.replace('http://localhost/copytube/register/register.html') // ref: https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
