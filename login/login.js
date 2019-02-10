9ef8b1d25dd3103ef2e02d46771f8268586f7a47
Edward <edward.bebbington@intercity.technology>
2019-02-10 01:59:03 +0000

    Literally cant remember what i did.. something like adding learnign.php and adding code from informat as commented todos

--- login/login.js
+++ login/login.js
@@ -1,5 +1,17 @@
 /* global $ */
 
+// todo :: implement lockout or reset user if tried login too many times
+/*
+password_tries_left--;
+                        if (password_tries_left == 0){
+                            alert("You will now be locked out.");
+                            $('*').prop('disabled', true);
+                        } else {
+                            alert("Please enter the correct credentials. You have " + password_tries_left + " tries left.");
+                            $('#input-full-name').text("") && $('#input-password').text("");
+                        }
+ */
+
 $(document).ready(function () {
   // On click of registering
   $(document).on('click', '#register-new-account', function () {
