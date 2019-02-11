50b2747999251bf28f238d2c9d8e144a8cd30a58
Edward <edward.bebbington@intercity.technology>
2019-02-11 20:09:12 +0000

    reverted back to orinal click events of ad button

--- models/save_comment.php
+++ models/save_comment.php
@@ -3,18 +3,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "password";
-$author = $_POST['']; // todo :: how to pass in cookie
-$comment = $_POST['comment-bar'];
-$date = $_POST['']; // todo :: create date
-$videoTitle = $_POST['videoTitle']; // todo :: how to get title
-// todo :: add in serverside validation using link: https://www.youtube.com/watch?time_continue=223&v=sgrvuMlf93w
-if (isset($_POST['submit'])) {
-    if (empty($author)) {
-        $error = "<p>Please enter name";
-    } else {
-        if (!preg_match('/'))
-    }
-}
+$author = $_POST['author'];
+$comment = $_POST['comment'];
+$date = $_POST['datePosted'];
+$videoTitle = $_POST['videoTitle'];
 //create connection
 $connection = new mysqli($servername, $username, $password, 'copytube');
 //check connection
