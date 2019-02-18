cd441fa5d46d29a63f0348be31e28ec0348a5a94
Edward <edward.bebbington@intercity.technology>
2019-02-18 01:45:32 +0000

    Started work on login again but it can return false from the ajax, needs further testing

--- login/login.js
+++ login/login.js
@@ -1,19 +1,44 @@
-/* global $ */
-
-/*
-password_tries_left--;
-                        if (password_tries_left == 0){
-                            alert("You will now be locked out.");
-                            $('*').prop('disabled', true);
-                        } else {
-                            alert("Please enter the correct credentials. You have " + password_tries_left + " tries left.");
-                            $('#input-full-name').text("") && $('#input-password').text("");
-                        }
- */
+/* global $, alert */
+let loginTries = 3
+function login () {
+  alert('running login fucntion')
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost/copytube/login/login.php',
+    data: {
+      email: $('#login-email').val(),
+      password: $('#login-password').val()
+    },
+    success: function (response) {
+      alert('running success: ' + response)
+      const loginStatus = JSON.parse(response)
+      if (loginStatus === false) {
+        alert('returned false')
+        loginTries--
+        if (loginTries === 0) {
+          window.close() // todo :: better lock out function
+          return false
+        } else {
+          $('#incorrect-credentials').text('Please enter the correct credentials. You have ' + loginTries + ' tries left')
+          return false
+        }
+      } else {
+        alert('returned true')
+        window.location.replace('http://localhost/copytube/index/copytube.php')
+        return false
+      }
+    },
+    error: function (error) {
+      alert('error: ' + error)
+      return false
+    }
+  })
+}
 
 $(document).ready(function () {
+  $(document)
   // On click of registering
   $(document).on('click', '#register-new-account', function () {
-    window.location.replace('http://localhost/copytube/register/register.php') // ref: https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+    window.location.replace('http://localhost/copytube/register/register.html') // ref: https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
   })
 })
--- login/login.php
+++ login/login.php
@@ -9,50 +9,37 @@
 $serverName = "localhost";
 $username = "root";
 $password = "password";
-$maxLength = 40;
-$email = $_POST['username'];
+$email = $_POST['email'];
 $passwordInput = $_POST['password'];
-
-// Validation
-if ($email === '' || $usernameInput >= ($maxLength + 1) || trim($usernameInput) === 0 || $usernameInput === null) {
-    print_r(false);
+//create connection to db
+$connection = new mysqli($serverName, $username, $password, 'copytube');
+//check connection to db
+if ($connection->connect_error) {
+    die("connection failed: " . $connection->connect_error);
+}
+//Get password from db
+$sql = "SELECT email, password FROM users WHERE email='$email'";
+//$result = query of $sql
+$result = $connection->query($sql);
+//If query fails, die. If not then get results
+if ($result == false){
+    print_r(json_encode(false));
 } else {
-    if ($passwordInput === '' || $passwordInput >= ($maxLength + 1) || trim($passwordInput) === 0 || $passwordInput === null) {
-        print_r(false);
+    $response = $result->fetch_all(MYSQLI_ASSOC);
+    // check if input is even in db
+    if (count($response) === 0) {
+        print_r(json_encode(false));
     } else {
-        //create connection to db
-        $connection = new mysqli($serverName, $username, $password, 'copytube');
-        //check connection to db
-        if ($connection->connect_error) {
-            die("connection failed: " . $connection->connect_error);
-        }
-        //Get password from db
-        $sql = "SELECT email, password FROM users WHERE email='$email'";
-        //$result = query of $sql
-        $result = $connection->query($sql);
-        //If query fails, die. If not then get results
-        if ($result == false){
+        //Compare DB and User Password
+        $passwordHash = password_hash($passwordInput, PASSWORD_BCRYPT);
+        if (password_verify($passwordInput, $response[0]['password'])) {
+            $sql = "UPDATE users SET loggedIn = 0 WHERE username = '$usernameInput'"; // todo :: remove me when user status is better
+            $connection->query($sql);
+            $connection->close();
+            setcookie(username, $_POST['username']);
+            print_r(json_encode(true));
         } else {
-            $response = $result->fetch_all(MYSQLI_ASSOC);
-            // check if input is even in db
-            if (count($response) === 0) {
-                echo "<script>alert('Incorrect credentials');</script>";
-                echo "<script>window.location.replace('http://localhost/copytube/login/login.html');</script>";
-            } else {
-                //Compare DB and User Password
-                $passwordHash = password_hash($passwordInput, PASSWORD_BCRYPT);
-                if (password_verify($passwordInput, $response[0]['password'])) {
-                    echo "<script>alert('password verification succeeded');</script>";
-                    $sql = "UPDATE users SET loggedIn = 0 WHERE username = '$usernameInput'";
-                    $connection->query($sql);
-                    $connection->close();
-                    $open = "<script>window.location.replace('http://localhost/copytube/index/copytube.php')</script>";
-                    echo $open;
-                    setcookie(username, $_POST['username']);
-                } else {
-                    echo "<script>alert('password not verified = wrong');</script>";
-                }
-            }
+            print_r(json_encode(false));
         }
     }
 }
