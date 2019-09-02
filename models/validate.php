403c4e5b20ac51a0d5d11a6bcd1111124f0d8514
ebebbington <EdwardSBebbington@hotmail.com>
2019-09-02 22:30:05 +0100

    completely refactored the validate model

--- models/validate.php
+++ models/validate.php
@@ -19,65 +19,92 @@ class Validate
     // SQL Queries
     //
     const SELECT_ALL_USERS = "SELECT * FROM users";
-
-    const GET_ALL_USERNAMES = "SELECT username FROM users";
-
-    const GET_ALL_EMAILS = "SELECT email_address FROM users";
+    const GET_USERNAMES_BY_USERNAME = "SELECT username FROM users WHERE username = ?";
+    const GET_EMAILS_BY_EMAIL = "SELECT email_address FROM users WHERE email_address = ?";
 
     //
     // Initialise data
     //
     private $usernameMaxLen = 40;
-
-    private $db;
-
+    private $commentMaxLen = 400;
     private $usernames;
+    private $passwordMinLen = 8;
 
     //
     // Initialise
     //
     public function __construct()
     {
-        $this->db = new Database();
     }
 
-    public function isSet($data = [])
+    /**
+     * Are a list of values set and valid
+     * 
+     * @param Array $data The arra to loop through checking each value
+     * @return Array [success, message, data] The response object
+     */
+    public function isSet(Array $data = [])
     {
         for ($i = 0; $i < (sizeof($data) - 1); $i++) {
-            if ( ! $data[ $i ] || ! isset($data[ $i ])) {
-                $result[ 'message' ] = 'Some data has not been set when passed into the request controller';
-
-                return [false, "A value in the data array hasnt been set", null];
+            if ( ! $data[ $i ] || ! isset($data[ $i ]) || trim($data[$i] < 1 || $data[$i] === null || empty($data[$i] ))) {
+                return [
+                    'success' => false,
+                    'message' => 'Some values are not set or dont contain anything',
+                    'data' => null
+                ];
                 break;
             }
         }
-
-        return [true, 'All values are set', null];
+        return [
+            'success' => true,
+            'message' => 'Values are set',
+            'data' => $data
+        ];
     }
 
-    public function validateComment($comment)
+    /**
+     * Prepare a string to display by sanitising it
+     * 
+     * @param String $value The string to check
+     * @return Array [success, message, data] The response object
+     */
+    public function prepareStringForView (String $value = '') {
+        $value = filter_var($string, FILTER_SANITIZE_STRING);
+        if (!$value) {
+            return ['success' => false, 'message' => 'Tags are not allowed', 'data' => null];
+        }
+        return = ['success' => true, 'message' => 'Sanitised the input', 'data' => $value];
+    }
+
+    /**
+     * Validate a comment
+     * 
+     * @param String $comment The comment the user added
+     * @return Array $result[success, message, data] The response object
+     */
+    public function comment($comment)
     {
+        $result = [
+            'success' => false,
+            'message' => '',
+            'data' => 'comment'
+        ];
         // Validate
-        if (strlen($comment) > 400 || trim($comment) === 0 || $comment === null
-          || empty($comment)
-          || ! isset($comment)
-        ) {
-            return [false, 'Could not validate comment', null];
+        $commentIsSet = $this->isSet($comment);
+        if ($commentIsSet['success'] === false) {
+            $result['message'] = 'Comment is not a valid string';
+            return $result;
         }
-        // Sanitise
-        if ( ! filter_var($comment, FILTER_SANITIZE_STRING)) {
-            return [false, 'Could not sanitise comment', null];
+        if (strlen($comment) > $this->commentMaxLen) {
+            $result['message'] = 'Comment is too long';
+            return $result
         }
-        try {
-            $this->db->openDatabaseConnection();
-            $comment = mysqli_real_escape_string($this->db->connection, $comment);
-            $this->db->closeDatabaseConnection();
-            return [true, 'Successfully validated comment', $comment];
-        } catch (Exception $e) {
-            new Mail('edward.bebbington@intercity.technology', 'Error: DB', $e);
-            return [false, 'Error produced when using the DB to sanitise comment', $e];
-        }
+        $result['success'] = true;
+        $result['message'] = 'Validated comment';
+        $result['data'] = $comment;
+        return $result;
     }
+
 
     /**
      * Validate and Sanitise a Username
@@ -86,23 +113,20 @@ class Validate
      * account, or for more further into the future
      * 
      * @param string $username The username field in the register form
-     * @return array $result Array holding the result information of function call
-     * bool ['success'] If the execution followed through as expected
-     * string ['message'] Message to be accompanied with the success
-     * any ['data'] data to be passed back containing needed information
+     * @return Array [success, message, data] The response object
      */
-    public function validateUsername($username = '')
+    public function validateUsername(String $username = '')
     {
         // Validate
-        if (strlen($username) > $this->usernameMaxLen || trim($username) === 0
-          || $username === null
-          || empty($username)
-          || ! isset($username)
-        ) {
-            return ['success' => false, 'message' => 'Username is either empty or too long', 'data' => 'username'];
+        $usernameIsSet = $this->isSet($username);
+        if (!$usernameIsSet) {
+            return ['success' => false, 'message' => 'Username is not set', 'data' => 'username'];
+        }
+        if (strlen($username) > $this->usernameMaxLen) {
+            return ['success' => false, 'message' => 'Username is too long', 'data' => 'username'];
         }
         // Check RegEx
-        if (preg_match("/^[a-zA-Z ]*$/", $username) !== 1) {
+        if (preg_match("/^[a-zA-Z ]*$/", $username) < 1) {
             return ['success' => false, 'message' => 'Only letters and whitespaces are allowed', 'data' => 'username'];
         }
         // Sanitise
@@ -112,10 +136,14 @@ class Validate
         }
         // Check if username exists
         $db = new Database();
-        $result = $db->runQuery(self::GET_ALL_USERNAMES)
+        $result = $db->runQuery(self::GET_USERNAMES_BY_USERNAME, [$username]);
         if ($result['success'] === false) {
             return $result;
         }
+        if ($result['rowCount'] === 1) { // username already exists
+            return ['success' => false, 'message' => 'Username already exists', 'data' => 'username'];
+        }
+        /* Old way before i added in a username param to the query
         $usernames = $result['data'];
         for ($i = 0, $l = sizeof($usernames); $i < $l; $i++) {
             // IM A GENIUS
@@ -127,7 +155,7 @@ class Validate
                 ];
                 break;
             }
-        }
+        } */
         return [
             'success' => true,
             'message' => 'Username successfuly validated',
@@ -147,7 +175,7 @@ class Validate
                 // $verifyEmail->Debug = true; // Creates an alert currently with the process
                 // $verifyEmail->Debugoutput = 'html'; // Displays js code error in console
                 $verifyEmail->setEmailFrom($email);
-            } catch (exception $e) {
+            } catch (exception $e
                 return ['email', 'Could not validate email address'];
             }
             if ($verifyEmail->check($email)) {
@@ -164,18 +192,26 @@ class Validate
         }
     }
 
-    public function validateEmail($email)
+    /**
+     * Validate and Sanitise an Email
+     * 
+     * @param string $email The email string to check
+     * @return array $result Array holding the result information of function call
+     * bool ['success'] If the execution followed through as expected
+     * string ['message'] Message to be accompanied with the success
+     * any ['data'] data to be passed back containing needed information
+     */
+    public function validateEmail(String $email = '')
     {
-        // Validate
-        if (trim($email) === 0 || $email === null || empty($email)) {
+        $emailIsSet = $this->isSet($email);
+        if ($email['success'] === false) {
             return [
                 'success' => false,
-                'message' => 'Enter a correct format',
+                'message' => 'Email cannot be empty or not set',
                 'data' => 'email'
             ];
         }
         // Sanitise
-        // todo :: use FILTER_VALIDATE_EMAIL too
         $email = filter_var($email, FILTER_VALIDATE_EMAIL);
         if (!$email) {
             return [
@@ -192,36 +228,19 @@ class Validate
                 'data' => 'email'
             ];
         }
-        // RegEx
-        // todo :: find a working email regex
-        // if (preg_match("[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]{2,}", $email) !== 1) {
-        //     return [
-        //         'success' => false,
-        //         'message' => 'Doesnt match expression',
-        //         'data' => 'email'
-        //     ];
-        // }
         // Check email against current emails
-        try {
-            // todo :: getting all emails is so inefficient smh... just get them WHERE email = ? you pleb
-            $this->db->openDatabaseConnection();
-            $query  = $this->db->connection->query(self::GET_ALL_EMAILS);
-            $emails = $query->fetch_all(MYSQLI_ASSOC);
-            for ($i = 0, $l = sizeof($emails); $i < $l; $i++) {
-                $existingEmail = $emails[1]['email_address'];
-                // IM A GENIUS
-                if ($email === $existingEmail) {
-                    return [
-                        'success' => false,
-                        'message' => 'Email already exists',
-                        'data' => 'email'
-                    ];
-                    break;
-                }
-            }
-        } catch (Exception $e) { var_dump($e); };
-        $email = mysqli_real_escape_string($this->db->connection, $email);
-
+        $db = new Database();
+        $result = $db->runQuery(self::GET_EMAILS_BY_EMAIL, [$email]);
+        if ($result['success'] === false) {
+            return $result;
+        }
+        if ($result['rowCount'] === 1) {
+            return [
+                'success' => false,
+                'message' => 'Email already exists',
+                'data' => 'email'
+            ];
+        }
         return [
             'success' => true,
             'message' => 'Scuessfully validated email',
@@ -229,35 +248,63 @@ class Validate
         ];
     }
 
-    public function compareStrings ($string1 = '', $string2 = '') {
-        if (!$string1 || !$string2) {
-            return true;
-        }
+    /**
+     * Check if 2 strings are the same or if one contains the other
+     * 
+     * @param String $string1 The first string to use
+     * @param String $string2 The second string to compare against
+     * @return Array The resulting object
+     */
+    public function compareStrings (String $string1 = '', String $string2 = '') {
         if ($string1 === $string2 || strpos($string2, $string1) !== false) {
-            return true;
+            return ['success' => true, 'message' => "$string1 is the same or contains $string2", 'data' => [$string1, $string2]];
         }
-        return false;
+        return ['success' => false, 'message' => "$string1 is not the same and does not contain $string2", 'data' => [$string1, $string2]];
     }
 
-    public function validatePassword($password)
+    /**
+     * Validate a password
+     * 
+     * Make checks against the password
+     * 
+     * @param String $password The password to check
+     * @return Array The resulting object
+     */
+    public function validatePassword(String $password = '')
     {
         // Validate
-        if (trim($password) === 0 || $password === null || empty($password)
-          || ! isset($password)
-        ) {
+        $passwordIsSet = $this->isSet($password);
+        if ($passwordIsSet['success'] === false) {
             return [
                 'success' => false,
                 'message' => 'Enter a password',
                 'data' => 'password'
             ];
         }
-        if (strlen($password) < 8) {
+        if (strlen($password) < $this->passwordMinLen) {
             return [
                 'success' => false,
                 'message' => 'Password must cotain 8 or more characters',
                 'data' => 'password'
             ];
         }
+        // Find a number
+        if (preg_match('~[0-9]~', $password) < 1) {
+            return [
+                'success' => false,
+                'message' => 'Password must contain at least 1 number',
+                'data' => 'password'
+            ];
+        }
+        // Find upper and lower case
+        if (preg_match('/[a-z][A-Z]/', $password) < 1) {
+            return [
+                'success' => false,
+                'message' => 'Must contain at least 1 or more uppercase and lowercase letters',
+                'data' => 'password'
+            ];
+        }
+        /* Old way before I solved the whole number situation with a single line... smh
         // Password - Find a number - personal algorithm
         $numberFound = false;
         for ($i = 0, $l = strlen($password); $i < $l; $i++) {
@@ -314,11 +361,11 @@ class Validate
                     'data' => $password
                 ];
             }
-        }
+        }*/
         return [
-            'success' => false,
-            'message' => 'Please enter at least one number',
-            'data' => 'password'
+            'success' => true,
+            'message' => 'Password successfully validated',
+            'data' => $password
         ];
     }
 }
