d129f9f0a51a61d594612fc730f42aeb6bbb6db7
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-11-16 00:58:18 +0000

    added a unit test for register and added test refs

--- src/tests/Unit/ExampleTest.php
+++ src/tests/Unit/ExampleTest.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Running tests
+ * 
+ * Be inside the project root and not the tests directory.
+ * Run vendor/bin/phpunit tests/Unit|Feature/Testfile.php
+ * 
+ * Writing a testable class
+ * 
+ * All methods must be prefixed with "test"
+ * 
+ */
+
 namespace Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
