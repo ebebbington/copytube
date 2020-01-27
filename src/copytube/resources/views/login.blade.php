7224a949d43842343c7b71cdfb464fe83136bd87
ebebbington <EdwardSBebbington@hotmail.com>
2020-01-27 01:54:02 +0000

    JP: FT - added autofocus for views

--- src/copytube/resources/views/login.blade.php
+++ src/copytube/resources/views/login.blade.php
@@ -11,7 +11,7 @@
         <fieldset>
             <legend><img class="form-logo" src="img/copytube_logo.png" alt="Logo"></legend>
             <legend class="form-title">Login</legend>
-            <label><input placeholder="jane.doe@hotmail.com" id="email" class="form-field form-control" type="email" name="email" required></label>
+            <label><input placeholder="jane.doe@hotmail.com" id="email" class="form-field form-control" type="email" name="email" required autofocus></label>
             <label><input id="password" class="form-field form-control" placeholder="Enter a password" type='password' name='password' required></label> <!-- https://www.w3schools.com/tags/att_input_pattern.asp -->
             <input id="login-button" class="btn btn-primary form-submit" type="button" name="submit" value="Submit">
             <a href="/register">Go To Register</a>
--- src/copytube/resources/views/register.blade.php
+++ src/copytube/resources/views/register.blade.php
@@ -11,7 +11,7 @@
         <fieldset>
             <legend><img class="form-logo" src="img/copytube_logo.png" alt="Logo"></legend>
             <legend class="form-title">Register</legend>
-            <label><input placeholder="Jane Doe" id="username" class="form-control form-field" type="text" name="username" required></label>
+            <label><input placeholder="Jane Doe" id="username" class="form-control form-field" type="text" name="username" required autofocus></label>
             <label><input placeholder="jane.doe@hotmail.com" id="email" class="form-field form-control" type="email" name="email" required></label>
             <label><input id="password" class="form-field form-control" placeholder="Enter a password" type='password' name='password' required></label> <!-- https://www.w3schools.com/tags/att_input_pattern.asp -->
             <input id="register-button" class="btn btn-primary form-submit" type="button" name="submit" value="Submit">
