42d39aba9a6a7874304b28cff3a1db70fddf56f3
Edward Bebbington <edward.bebbington@intercity.technology>
2020-03-27 20:29:09 +0000

    Improved redis handling

--- src/realtime/app.ts
+++ src/realtime/app.ts
@@ -1,29 +1,40 @@
 // TODO 4 :: Rework this whole aspect to support the handling of redis connections and sending of socket messages to the client
 import SocketServer from "./server.ts";
+import { connect } from "./deps.ts";
+
+const redis = await connect({
+    hostname: "copytube_redis",
+    port: 6379
+});
 const io = new SocketServer();
+
 io.on('connection', () => {
     console.log('A user connected.');
 });
 io.on('chatroom1', function (incomingMessage: any) {
+    console.log('message from chatroom1')
     io.to('chatroom1', incomingMessage);
 });
 io.on('disconnect', () => {
     console.log('A user disconnected.');
 });
 
-import { connect } from "./deps.ts";
-const redis = await connect({
-  hostname: "copytube_redis",
-  port: 6379
-});
-const ok = await redis.set("hoge", "fuga");
-const fuga = await redis.get("hoge");
 
-// TODO 1 :: Create a conf list of channels to listen on. check if an array can be passed in
-const sub = await redis.subscribe("test-channel");
-(async function() {
-  for await (const { channel, message } of sub.receive()) {
-    // on message
-    console.log('received a message through redis on channel test-channel:' + message)
-  }
-})();
+class Redis {
+    public static async listen () {
+        const sub = await redis.subscribe('realtime.comments.new');
+        (async () => {
+            for await (const { channel, message } of sub.receive()) {
+                // on message
+                console.log('MESSAGE RECEIEVD FROM CLASS:')
+                console.log(message)
+                // TODO :: Call socket method to send. eg `io.send(channel, message)`
+            }
+        })();
+    }
+    private static async playground () {
+        const ok = await redis.set("hoge", "fuga");
+        const fuga = await redis.get("hoge");
+    }
+}
+await Redis.listen()
