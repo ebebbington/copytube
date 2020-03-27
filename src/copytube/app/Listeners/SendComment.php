0bb7f482baf474f41aa230e99a23899aa07583e8
Edward Bebbington <edward.bebbington@intercity.technology>
2020-03-27 12:36:39 +0000

    Added a listen class to handle event for new comment

---
+++ src/copytube/app/Listeners/SendComment.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CommentAdded;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+
+class SendComment
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  CommentAdded  $event
+     * @return void
+     */
+    public function handle(CommentAdded $event)
+    {
+        Log::info('[SendComment Listener - handle] Sending the comment to realtime.comments.new');
+        Redis::publish('realtime.comments.new', json_encode($event->comment));
+    }
+}
