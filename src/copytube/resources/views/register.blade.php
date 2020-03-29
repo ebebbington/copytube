f92c75737888dc204ed4cff7438764745ef6506d
Edward Bebbington <edward.bebbington@intercity.technology>
2020-03-29 02:47:29 +0100

    Added file input field

--- src/copytube/resources/views/register.blade.php
+++ src/copytube/resources/views/register.blade.php
@@ -10,7 +10,8 @@
         <label><input placeholder="Jane Doe" id="username" class="form-control form-field" type="text" name="username" required autofocus></label>
         <label><input placeholder="jane.doe@hotmail.com" id="email" class="form-field form-control" type="email" name="email" required></label>
         <label><input id="password" class="form-field form-control" placeholder="Enter a password" type='password' name='password' required></label> <!-- https://www.w3schools.com/tags/att_input_pattern.asp -->
+        <label><input id="profile-picture" class="form-field form-control" placeholder="Profile Picture" type="file" name="profile-picture"></label>
         <input id="register-button" class="btn btn-primary form-submit" type="button" name="submit" value="Submit">
     @endcomponent
 </div>
-@stop    
+@stop
