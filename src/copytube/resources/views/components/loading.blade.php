58f2f19908d9724a647c6025ea12ae6cf8a2241a
Edward Bebbington <edward.bebbington@intercity.technology>
2020-03-29 02:28:03 +0100

    Added html for loading circles

--- src/copytube/resources/views/components/loading.blade.php
+++ src/copytube/resources/views/components/loading.blade.php
@@ -1,4 +1,8 @@
 <div id="overlay-container"></div>
 <div id="loading-container">
-    <p></p>
+    <i id="loading-circle-one" class="circle loading-circles"></i>
+    <i id="loading-circle-two" class="circle loading-circles"></i>
+    <i id="loading-circle-three" class="circle loading-circles"></i>
+    <i id="loading-circle-four" class="circle loading-circles"></i>
+    <i id="loading-circle-five" class="circle loading-circles"></i>
 </div>
