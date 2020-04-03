e89d9e7e98a784d8f8c8847e980a65935a99baa1
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-03 18:38:38 +0100

    Created a laravel email class related to accounts being locked

---
+++ src/copytube/app/Mail/AccountLocked.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
+
+class AccountLocked extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $title;
+
+    public $body;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($title, $body)
+    {
+        $this->title = $title;
+        $this->body = $body;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.user.locked');
+    }
+}
