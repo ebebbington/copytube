5423ce7dcec15378a944aa3f6e92bf9a3b4803bf
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-04 15:42:25 +0100

    Got mail working

--- src/copytube/app/Mail/AccountLocked.php
+++ src/copytube/app/Mail/AccountLocked.php
@@ -34,6 +34,8 @@ class AccountLocked extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.user.locked');
+        return $this
+            ->from('ebebbington.copytube@gmail.com')
+            ->view('emails.user.locked');
     }
 }
