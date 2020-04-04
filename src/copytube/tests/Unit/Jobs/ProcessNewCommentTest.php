925835471bc8afb11a5956bbcf2f658d64f54178
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-04 16:06:23 +0100

    Completed unit test for process new comment

--- src/copytube/tests/Unit/Jobs/ProcessNewCommentTest.php
+++ src/copytube/tests/Unit/Jobs/ProcessNewCommentTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Unit\Jobs;
 
+use App\CommentsModel;
+use App\Jobs\ProcessNewComment;
+use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
 class ProcessNewCommentTest extends TestCase
@@ -11,8 +14,32 @@ class ProcessNewCommentTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testProcessNewComment()
     {
-        $this->assertTrue(true);
+        Queue::fake();
+
+        // Assert that no jobs were pushed...
+        Queue::assertNothingPushed();
+
+        // Get data
+        $CommentsModel = new CommentsModel;
+        $comment = $CommentsModel->CreateQuery([
+            'comment' => 'Test',
+            'author' => 'Test',
+            'date_posted' => '2020-02-02',
+            'user_id' => 2,
+            'video_posted_on' => 'test'
+        ]);
+
+//        Queue::assertPushed(ProcessNewComment::class, function ($job) {
+//            return 1 === 10;
+//        });
+
+        // Run the faked job
+        $job = (new ProcessNewComment($comment, 'img/test'))->onQueue('comments');
+        dispatch($job);
+
+        // Expect it was called
+        Queue::assertPushedOn('comments', ProcessNewComment::class);
     }
 }
