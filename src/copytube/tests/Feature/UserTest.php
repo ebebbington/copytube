138f83e46830e4438c94c1ea85c02070f603d764
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-06 17:34:05 +0100

    Improved user controller test following new test utilities class

--- src/copytube/tests/Feature/UserTest.php
+++ src/copytube/tests/Feature/UserTest.php
@@ -6,25 +6,13 @@ use App\UserModel;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use PHPUnit\Util\Test;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
-    private function createUserAndGetId ()
-    {
-        DB::table('users')->insert([
-            'username' => 'TestUsername',
-            'email_address' => 'TestEmail@hotmail.com',
-            'password' => UserModel::generateHash('TestPassword1'),
-            'login_attempts' => 3,
-            'logged_in' => 0
-        ]);
-        $userId = DB::table('users')->select('id')->where('email_address', '=', 'TestEmail@hotmail.com')->first();
-        $userId = $userId->id;
-        return $userId;
-    }
     public function testDeleteMethodWithoutAuth ()
     {
         // Test request when not authed - should fail
@@ -38,17 +26,16 @@ class UserTest extends TestCase
     public function testDeleteMethodWithAuthOnSuccess ()
     {
         // Copy profile picture
-        $userId = $this->createUserAndGetId();
+        $userId = TestUtilities::createTestUserInDb();
         $profilePicPath = 'img/'.$userId.'/test.jpg';
         Storage::disk('local_public')->copy('img/sample.jpg', $profilePicPath);
         // Update user in table
         DB::table('users')
-            ->where('email_address', '=', 'TestEmail@hotmail.com')
+            ->where('email_address', '=', TestUtilities::$validEmail)
             ->update(['profile_picture' => $profilePicPath]);
 
         // Auth myself as its an authed route
-        $user = DB::table('users')->where('email_address', '=', 'TestEmail@hotmail.com')->first();
-        Auth::loginUsingId($user->id);
+        Auth::loginUsingId($userId);
         $user = Auth::user();
 
         // Add comments before
@@ -56,14 +43,14 @@ class UserTest extends TestCase
             [
                 'comment' => 'Hello',
                 'author' => 'hello',
-                'user_id' => $user->id,
+                'user_id' => $userId,
                 'video_posted_on' => 'test',
                 'date_posted' => '2020-03-03'
             ],
             [
                 'comment' => 'Hello',
                 'author' => 'hello',
-                'user_id' => $user->id,
+                'user_id' => $userId,
                 'video_posted_on' => 'test',
                 'date_posted' => '2020-03-03'
             ]
@@ -80,11 +67,11 @@ class UserTest extends TestCase
         //Storage::disk('local_public')->assertMissing($profilePicPath);
 
         // Check row was deleted
-        $row = DB::table('users')->where('email_address', '=', 'TestEmail@hotmail.com')->first();
+        $row = TestUtilities::getTestUserInDb();
         $this->assertEquals(false, $row);
 
         // Check all comments were deleted
-        $comments = DB::table('comments')->where('user_id', '=', $user->id)->get();
+        $comments = DB::table('comments')->where('user_id', '=', $userId)->get();
         $this->assertEquals(false, sizeof($comments) >= 1);
 
         // Check we are no longer authed
