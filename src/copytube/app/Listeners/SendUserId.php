84c29512a2d826be5ecee4dc84233db5f10848da
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-08 07:51:47 +0100

    Added send user id listener

---
+++ src/copytube/app/Listeners/SendUserId.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserDeleted;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Redis;
+
+class SendUserId
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param UserDeleted $event
+     *
+     * @return void
+     */
+    public function handle(UserDeleted $event)
+    {
+        Redis::publish($event->channel, json_encode([
+                'channel' => $event->channel,
+                'type' => $event->type,
+                'userId' => $event->userId]
+        ));
+    }
+}
