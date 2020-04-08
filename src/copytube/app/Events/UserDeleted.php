c52571196b3db01d89b7de666b19da8cce55c105
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-08 07:51:02 +0100

    Added user deleted event

---
+++ src/copytube/app/Events/UserDeleted.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserDeleted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public int $userId;
+    public string $type;
+    public string $channel;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param int $userId
+     */
+    public function __construct(int $userId)
+    {
+        $this->userId = $userId;
+        $this->type = 'delete';
+        $this->channel = 'realtime.users.delete';
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
