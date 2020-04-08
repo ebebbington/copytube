f6ed9873aca8a5b49ff2cf694c0d7483927d9b24
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-08 20:30:58 +0100

    Created unnit test for socket class

---
+++ src/realtime/tests/socket_test.ts
@@ -0,0 +1,70 @@
+import {assertEquals, connectWebSocket} from "../deps.ts"
+import Redis from "../redis.ts";
+import SocketServer from "../socket.ts";
+
+// Deno.test({
+//     name: 'The Socket Server Should Add to the List Of Clients',
+//     async fn(): Promise<any> {
+//         let allClients = SocketServer.getAllClients()
+//         assertEquals(0, allClients.length)
+//         console.log('connecting from test')
+//         const client1 = await connectWebSocket("ws://127.0.0.1:9008/realtime");
+//         allClients = SocketServer.getAllClients()
+//         console.log('connected, heres list:')
+//         console.log(allClients)
+//         assertEquals(1, allClients.length)
+//     }
+// })
+
+// Deno.test({
+//     name: 'The Socket Server Should Remove from The List Of Clients',
+//     async fn(): Promise<any> {
+//         const client1 = await connectWebSocket("ws://127.0.0.1:9008/realtime");
+//         let allClients = SocketServer.getAllClients()
+//         assertEquals(1, allClients.length)
+//         await client1.close()
+//         assertEquals(0, allClients.length)
+//     }
+// })
+
+Deno.test({
+    name: 'The Socket Server Should Recieve and Send a Message to the Client',
+    async fn(): Promise<any> {
+        // Create the socket client
+        const client = await connectWebSocket("ws://127.0.0.1:9008/realtime");
+        const msgToSend = 'Hello from Client :)';
+        // listen for recieved messages when they come in
+        (async function (): Promise<void> {
+            for await (const msg of client.receive()) {
+                assertEquals(msgToSend, msg)
+                await client.close(1000);
+            }
+        })();
+        // Sennd a message through redis so the socket server can send it to us
+        const redis = await Redis.connect()
+        const pub = await Redis.connect()
+        await pub.publish('realtime.comments.new', msgToSend)
+    }
+})
+
+Deno.test({
+    name: 'The Socket Server Should Send the Same Message to All Clients',
+    async fn(): Promise<any> {
+        // Create the socket client
+        const client1 = await connectWebSocket("ws://127.0.0.1:9008/realtime");
+        const client2 = await connectWebSocket("ws://127.0.0.1:9008/realtime");
+        const msgToSend = 'Hello from Client1 :)';
+        // listen for recieved messages when they come in
+        (async function (): Promise<void> {
+            for await (const msg of client2.receive()) {
+                assertEquals(msgToSend, msg)
+                await client1.close()
+                await client2.close()
+            }
+        })();
+        // Sennd a message through redis so the socket server can send it to us
+        const redis = await Redis.connect()
+        const pub = await Redis.connect()
+        await pub.publish('realtime.comments.new', msgToSend)
+    }
+})
