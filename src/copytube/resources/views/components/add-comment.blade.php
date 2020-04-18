3a7b9c0bc35e4adbb5a3e8b84304fdbcc326a30c
ebebbington <EdwardSBebbington@hotmail.com>
2020-04-18 03:19:16 +0100

    Fixed a few errors when posting a comment to support browser testing

--- src/copytube/resources/views/components/add-comment.blade.php
+++ src/copytube/resources/views/components/add-comment.blade.php
@@ -1,7 +1,7 @@
 <div id="comment">
     <span class="flex">
-        <textarea id="add-comment-input" contenteditable="true" type="text" class="form-control" placeholder="Add a Comment" aria-label="Add a comment"></textarea>
-        <p>0</p>
+        <textarea name="new-comment" id="add-comment-input" contenteditable="true" type="text" class="form-control" placeholder="Add a Comment" aria-label="Add a comment"></textarea>
+        <p id="comment-character-count">0</p>
     </span>
     <button type="button" class="btn">Send</button>
 </div>
