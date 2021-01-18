578cc75a93cdefda20dbd2028106581ec7b3019a
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-18 01:07:16 +0000

    chore(copytube): fix loading component

--- src/copytube/resources/views/components/loading.blade.php
+++ src/copytube/resources/views/components/loading.blade.php
@@ -1,8 +1,9 @@
 <div id="overlay-container"></div>
 <div id="loading-container">
-    <i id="loading-circle-one not-flex" class="circle loading-circles"></i>
-    <i id="loading-circle-two not-flex" class="circle loading-circles"></i>
-    <i id="loading-circle-three not-flex" class="circle loading-circles"></i>
-    <i id="loading-circle-four not-flex" class="circle loading-circles"></i>
-    <i id="loading-circle-five not-flex" class="circle loading-circles"></i>
+    <span id="loading-circle"></span>
+{{--    <i id="loading-circle-one not-flex" class="circle loading-circles"></i>--}}
+{{--    <i id="loading-circle-two not-flex" class="circle loading-circles"></i>--}}
+{{--    <i id="loading-circle-three not-flex" class="circle loading-circles"></i>--}}
+{{--    <i id="loading-circle-four not-flex" class="circle loading-circles"></i>--}}
+{{--    <i id="loading-circle-five not-flex" class="circle loading-circles"></i>--}}
 </div>
