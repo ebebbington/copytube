90f29ec0db187b4dfb9c863933c0651a989e7af9
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-19 01:00:43 +0000

    chore(copytube): redesign header

--- src/copytube/resources/views/components/header.blade.php
+++ src/copytube/resources/views/components/header.blade.php
@@ -1,21 +1,17 @@
-<div id="header-main">
-    <h1><img src="img/copytube_logo.png" alt="CopyTube Logo"></h1>
+<h1><img src="img/copytube_logo.png" alt="CopyTube Logo"></h1>
+<a class="menu-item" href="/home">Home</a>
+<a class="menu-item" href="/register">Register</a>
+<a class="menu-item" href="/login">Login</a>
+<a class="menu-item" href="/chat">Chat</a>
+@if ($username !== '')
+<img id="account-options" class="profile-picture" src="{{ $profilePicture }}" alt="Profile Picture">
+<div class="hide gear-dropdown">
+    <p class="bold">{{ $username }}</p>
+    <p>{{ $email }}</p>
+    <hr>
+    <ul>
+        <li><a href="/logout">Logout</a></li>
+        <li id="delete-account-trigger">Delete</li>
+    </ul>
 </div>
-<div id="header-sub">
-    <a class="menu-item" href="/home">Home</a>
-    <a class="menu-item" href="/register">Register</a>
-    <a class="menu-item" href="/login">Login</a>
-    <a class="menu-item" href="/chat">Chat</a>
-    @if ($username !== '')
-    <img id="account-options" class="profile-picture" src="{{ $profilePicture }}" alt="Profile Picture">
-    <div class="hide gear-dropdown">
-        <p class="bold">{{ $username }}</p>
-        <p>{{ $email }}</p>
-        <hr>
-        <ul>
-            <li><a href="/logout">Logout</a></li>
-            <li id="delete-account-trigger">Delete</li>
-        </ul>
-    </div>
-    @endif
-</div>
+@endif
