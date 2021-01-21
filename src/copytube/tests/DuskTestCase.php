4cb0453e591bed5746c7dd181ca6eacf17459d09
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-21 00:27:58 +0000

    debug

--- src/copytube/tests/DuskTestCase.php
+++ src/copytube/tests/DuskTestCase.php
@@ -37,7 +37,7 @@ abstract class DuskTestCase extends BaseTestCase
         ]);
 
         return RemoteWebDriver::create(
-            "http://copytube_selenium:4444/wd/hub",
+            "http://copytube_selenium:8999/wd/hub",
             DesiredCapabilities::chrome()->setCapability(
                 ChromeOptions::CAPABILITY,
                 $options
