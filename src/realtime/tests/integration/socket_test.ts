4114694d81286f7ec11954ba70aadf67ac536b09
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-08-20 18:19:10 +0100

    fix

--- src/realtime/tests/integration/socket_test.ts
+++ src/realtime/tests/integration/socket_test.ts
@@ -15,22 +15,25 @@ Deno.test(
   "int/socket | Messages | The Socket Server Should Receive a message from redis whenn one is published, and should send the message to the Client",
   async () => {
     // Create the socket client
-    const promise = deferred();
     const client = new WebSocket("ws://127.0.0.1:9008/realtime");
     const pub = await Redis.connect();
     const msgToSend = "Hello from Client :)";
     // listen for recieved messages when they come in
+    let msgGotten = ""
+    const p1 = deferred()
     client.onmessage = function (msg) {
-      assertEquals(msgToSend, msg.data);
-      client.close();
+      msgGotten = msg.data
+      p1.resolve()
     };
-    client.onclose = function () {
-      pub.close();
-      promise.resolve();
-    };
-    // Sennd a message through redis so the socket server can send it to us
     await pub.publish("realtime.comments.new", msgToSend);
-    await promise;
+    await p1
+    const promise = deferred();
+    client.onclose = () => promise.resolve();
+    client.close()
+    await promise
+    pub.close()
+    console.log(msgGotten, msgToSend)
+    assertEquals(msgGotten, msgToSend)
   },
 );
 
