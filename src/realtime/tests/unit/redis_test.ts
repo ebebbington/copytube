1b3022b23013748200f134293dd78c192f192b49
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-08-20 18:12:21 +0100

    fix

--- src/realtime/tests/unit/redis_test.ts
+++ src/realtime/tests/unit/redis_test.ts
@@ -1,41 +1,31 @@
 import { deferred, Rhum } from "../deps.ts";
 import { Redis } from "../../redis.ts";
 
-Rhum.testPlan("tests/unit/redis_test.ts", () => {
-  Rhum.testSuite("connect()", () => {
-    Rhum.testCase("Can connect to our Redis container", async () => {
-      const redis = await Redis.connect();
-      Rhum.asserts.assertEquals(redis.isConnected, true);
-      redis.close();
-    });
-  });
-  Rhum.testSuite("createSubscriber()", () => {
-    Rhum.testCase("Can create a subscriber", async () => {
-      const redis = await Redis.connect();
-      const sub = await Redis.createSubscriber(redis);
-      Rhum.asserts.assertEquals(sub.isClosed, false);
-      redis.close();
-    });
-  });
-  Rhum.testSuite("listen()", () => {
-    Rhum.testCase(
-      "Will call the callback when a message is sent to redis",
-      async () => {
-        const redis = await Redis.connect();
-        const sub = await Redis.createSubscriber(redis);
-        const pub = await Redis.connect();
-        const p = deferred<string>();
-        await Redis.listen(sub, (message: string) => {
-          p.resolve(message);
-        });
-        await pub.publish("realtime.comments.new", "wayway");
-        const message = await p;
-        redis.close();
-        pub.close();
-        Rhum.asserts.assertEquals(message, "wayway");
-      },
-    );
-  });
+Deno.test("unit/redis | connect() | Can connect to our Redis container", async () => {
+  const redis = await Redis.connect();
+  Rhum.asserts.assertEquals(redis.isConnected, true);
+  redis.close();
 });
-
-Rhum.run();
+Deno.test("unit/redis | createSubscriber() | Can create a subscriber", async () => {
+  const redis = await Redis.connect();
+  const sub = await Redis.createSubscriber(redis);
+  Rhum.asserts.assertEquals(sub.isClosed, false);
+  redis.close();
+});
+Deno.test(
+  "unit/redis | listen() | Will call the callback when a message is sent to redis",
+  async () => {
+    const redis = await Redis.connect();
+    const sub = await Redis.createSubscriber(redis);
+    const pub = await Redis.connect();
+    const p = deferred<string>();
+    await Redis.listen(sub, (message: string) => {
+      p.resolve(message);
+    });
+    await pub.publish("realtime.comments.new", "wayway");
+    const message = await p;
+    redis.close();
+    pub.close();
+    Rhum.asserts.assertEquals(message, "wayway");
+  },
+);
