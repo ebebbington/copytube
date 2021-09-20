3d8d5a453cec36730a6ac318ef0bf997eaca21ca
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-09-20 20:44:23 +0100

    Update update_deno_versions.ts

--- console/update_deno_versions.ts
+++ console/update_deno_versions.ts
@@ -4,5 +4,5 @@ const res = await fetch(
 const version = await res.json();
 const latestVersion = version.latest.replace("v", "");
 let dockerFileContent = new TextDecoder().decode(Deno.readFileSync("./.docker/drash.dockerfile"))
-dockerFileContent = dockerFileContent.replace(/-s v[0-9].*.*/, `-s v${latestVersion}`)
+dockerFileContent = dockerFileContent.replace(/-s v[0-9].*.*/, `-s v${latestVersion} \\`)
 Deno.writeFileSync("./.docker/drash.dockerfile", new TextEncoder().encode(dockerFileContent))
